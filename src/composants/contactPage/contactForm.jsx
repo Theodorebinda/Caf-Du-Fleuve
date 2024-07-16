c64a949a1338474a6c1fd0c99979ed215f591a1c
Théodorebinda <theodorebinda@gmail.com>
2024-07-16 17:38:09 +0100

    fix:contact integration

--- src/composants/contactPage/contactForm.jsx
+++ src/composants/contactPage/contactForm.jsx
@@ -9,6 +9,7 @@ const ContactForm = () => {
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
   const [message, setMessage] = useState("");
   const [errors, setErrors] = useState({});
   const [showSuccessPopup, setShowSuccessPopup] = useState(false);
@@ -33,6 +34,17 @@ const ContactForm = () => {
       };
     } else if (!validateEmail(email)) {
       formErrors = { ...formErrors, email: "Revoyez votre adresse email" };
+    }
+    if (!phone) {
+      formErrors = {
+        ...formErrors,
+        phone: "Veuillez entrer votre numéro de téléphone.",
+      };
+    } else if (!/^[0-9]{10}$/.test(phone)) {
+      formErrors = {
+        ...formErrors,
+        phone: "Veuillez entrer un numéro de téléphone valide.",
+      };
     }
 
     if (!message) {
@@ -66,23 +78,23 @@ const ContactForm = () => {
   };
 
   return (
-    <div className="bg-white">
-      <Localisation />
-      <form ref={form} onSubmit={sendEmail} className="">
-        <div className="">
-          <div className="">
-            <label className="">Nom</label>
+    <div className="flex flex-col bg-white p-4 md:p-10 m-3 md:m-10 rounded-md">
+      <Localisation className={"mb-10 md:mb-20"} />
+      <form ref={form} onSubmit={sendEmail} className=" flex flex-col gap-4">
+        <div className="flex flex-col md:flex-row md:justify-between md:gap-8 items-start md:items-center w-full">
+          <div className="flex flex-col gap-2 md:w-1/2 w-full mb-4">
+            <label className="font-semibold">Nom</label>
             <input
               type="text"
               name="name"
               placeholder="Entrez Votre nom"
-              className="bg-transparent focus:outline-none focus:border-b-2 pb-5 border-b"
+              className=" focus:outline-none focus:border-b-2 pb-5 border-b md:w-full"
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
             {errors.name && <span className="text-red-500">{errors.name}</span>}
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2 md:w-1/2 w-full mb-4">
             <label className="font-semibold">Email</label>
             <input
               type="email"
@@ -93,9 +105,21 @@ const ContactForm = () => {
               onChange={(e) => setEmail(e.target.value)}
             />
             {errors.email && (
-              <span className="text-red-500">{errors.email}</span>
+              <span className="text-red-400">{errors.email}</span>
             )}
           </div>
+        </div>
+        <div className="flex flex-col gap-2 md:w-1/2 w-full mb-4">
+          <label className="font-semibold">Téléphone</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Entrez votre numéro de téléphone"
+            className="bg-transparent focus:outline-none focus:border-b-2 pb-5 border-b"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          {errors.phone && <span className="text-red-400">{errors.phone}</span>}
         </div>
         <div className="flex flex-col gap-2">
           <label className="font-semibold">Message</label>
@@ -107,10 +131,14 @@ const ContactForm = () => {
             onChange={(e) => setMessage(e.target.value)}
           />
           {errors.message && (
-            <span className="text-red-500">{errors.message}</span>
+            <span className="text-red-400">{errors.message}</span>
           )}
         </div>
-        <input type="submit" value="Envoyer" className="" />
+        <input
+          type="submit"
+          value="Envoyer"
+          className=" md:w-1/4  bg-[#3E6100] text-white font-semibold hover:bg-[#140100] hover:text-[#588b00] py-3 px-5 rounded-lg md:m-auto"
+        />
       </form>
       {showSuccessPopup && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
--- src/composants/localisation.jsx
+++ src/composants/localisation.jsx
@@ -1,34 +1,37 @@
 import React from "react";
 import { CiLocationOn, CiMail } from "react-icons/ci";
+import { FiPhone } from "react-icons/fi";
 
-function Localisation() {
+function Localisation({ className }) {
   return (
-    <div>
-      <div className="flex flex-col md:flex-row md:justify-between  md:items-center">
-        <div className="flex justify-between gap-2 items-center">
+    <div className={`${className}`}>
+      <div
+        className={`flex flex-col md:flex-row md:justify-between gap-2  md:items-center`}
+      >
+        <div className="flex justify-start md:justify-between gap-2 items-center">
           <div className="p-6 bg-[#f6f6f6]">
             <CiMail size={40} />
           </div>
           <div className="flex flex-col justify-center">
-            <span className="font-bold">ADRESSE EMAIL :</span>
+            <span className="font-bold">ADRESSE EMAIL</span>
             <a href="mailto:cafedufleuve@gmail.com">cafedufleuver@gmail.com</a>
           </div>
         </div>
-        <div className="flex justify-between gap-2 items-center">
+        <div className="flex justify-start md:justify-between gap-2 items-center">
           <div className="p-6 bg-[#f6f6f6]">
             <CiLocationOn size={40} />
           </div>
           <div className="flex flex-col justify-center">
-            <span className="font-bold">NOS EMPLACEMENTS :</span>
+            <span className="font-bold">NOS EMPLACEMENTS</span>
             <a href="">Concession Chanic Metal</a>
           </div>
         </div>
-        <div className="flex justify-between gap-2 items-center">
+        <div className="flex justify-start md:justify-between gap-2 items-center">
           <div className="p-6 bg-[#f6f6f6]">
-            <CiMail size={40} />
+            <FiPhone className="font-light" size={40} />
           </div>
           <div className="flex flex-col justify-center">
-            <span className="font-bold">NUMERO DE TELEPHONE :</span>
+            <span className="font-bold">NUMERO DE TELEPHONE</span>
             <a href="tel:+243894594411">+243000099999</a>
             <a href="tel:+243894594411">+2430088899889</a>
           </div>
